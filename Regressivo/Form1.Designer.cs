c9ccdce82ea188a652123c5f77ccb4ec7f9fa5fd
guimaraf <guimaraf@gmail.com>
2017-04-19 18:20:52 -0300

    atualizando

---
+++ Regressivo/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Regressivo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBoxHoras = new System.Windows.Forms.TextBox();
+            this.txtBoxMinutos = new System.Windows.Forms.TextBox();
+            this.txtBoxSegundos = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(82, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Horas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(227, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Munutos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(368, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 24);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Segundos";
+            // 
+            // txtBoxHoras
+            // 
+            this.txtBoxHoras.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxHoras.Location = new System.Drawing.Point(68, 125);
+            this.txtBoxHoras.Name = "txtBoxHoras";
+            this.txtBoxHoras.Size = new System.Drawing.Size(100, 29);
+            this.txtBoxHoras.TabIndex = 3;
+            this.txtBoxHoras.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBoxHoras_KeyPress);
+            // 
+            // txtBoxMinutos
+            // 
+            this.txtBoxMinutos.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxMinutos.Location = new System.Drawing.Point(220, 125);
+            this.txtBoxMinutos.Name = "txtBoxMinutos";
+            this.txtBoxMinutos.Size = new System.Drawing.Size(100, 29);
+            this.txtBoxMinutos.TabIndex = 2;
+            this.txtBoxMinutos.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBoxMinutos_KeyPress);
+            // 
+            // txtBoxSegundos
+            // 
+            this.txtBoxSegundos.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxSegundos.Location = new System.Drawing.Point(372, 125);
+            this.txtBoxSegundos.Name = "txtBoxSegundos";
+            this.txtBoxSegundos.Size = new System.Drawing.Size(100, 29);
+            this.txtBoxSegundos.TabIndex = 1;
+            this.txtBoxSegundos.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBoxSegundos_KeyPress);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(211, 174);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(131, 29);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.Text = "Resultado";
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(72, 229);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 40);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(222, 229);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(100, 40);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(370, 229);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(100, 40);
+            this.btnLimpar.TabIndex = 6;
+            this.btnLimpar.Text = "Clear";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 321);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtBoxSegundos);
+            this.Controls.Add(this.txtBoxMinutos);
+            this.Controls.Add(this.txtBoxHoras);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(573, 360);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(573, 360);
+            this.Name = "Form1";
+            this.Text = "Regressivo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBoxHoras;
+        private System.Windows.Forms.TextBox txtBoxMinutos;
+        private System.Windows.Forms.TextBox txtBoxSegundos;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ Regressivo/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Regressivo
+{
+    public partial class Form1 : Form
+    {
+        public int horas, minutos, segundos, soma;
+        public bool relogioFunciona = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            relogioFunciona = true;
+            timer1.Interval = 1000;
+
+            if (txtBoxHoras.Text == "")
+            {
+                horas = 0;
+            }
+            else
+            {
+                horas = Convert.ToInt32(txtBoxHoras.Text);
+            }
+            
+            if(txtBoxMinutos.Text == "")
+            {
+                minutos = 0;
+            }
+            else
+            {
+                minutos = Convert.ToInt32(txtBoxMinutos.Text);
+            }
+            
+            if(txtBoxSegundos.Text == "")
+            {
+                segundos = 0;
+            }
+            else
+            {
+                segundos = Convert.ToInt32(txtBoxSegundos.Text);
+            }
+
+            soma = (horas * 3600) + (minutos * 60) + segundos;
+            timer1.Start();
+        }
+
+        private void txtBoxHoras_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 08)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtBoxMinutos_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 08)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtBoxSegundos_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 08)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            relogioFunciona = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            soma -= 1;
+            lblResultado.Text = Convert.ToString(soma);
+
+            if(soma == 0)
+            {
+                timer1.Stop();
+                relogioFunciona = false;
+                limparDados();
+                rodar();
+            }
+        }
+        
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            limparDados();
+        }
+        public void limparDados()
+        {
+            if (!relogioFunciona)
+            {
+                horas = 0;
+                minutos = 0;
+                segundos = 0;
+                soma = 0;
+                lblResultado.Text = "Resultado";
+                txtBoxHoras.Text = "";
+                txtBoxMinutos.Text = "";
+                txtBoxSegundos.Text = "";
+            }
+        }
+        public void rodar()
+        {
+            //Colocar evento externo para rodar aqui
+            MessageBox.Show("Mensagem de teste");
+        }
+    }
+}
